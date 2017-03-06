ba3c4dc14c7303acd8f0bc7c2e895a3c4148fee8
santosh <santospandey333@gmail.com>
2017-03-06 18:57:33 +0545

    loading texture

--- display.cpp
+++ display.cpp
@@ -1,6 +1,76 @@
 #include "display.h"
 
-void Display::show_test(){
+bool Display::init(){
 
-    std::cout<<"this is testing";
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else{
+        //Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		//Create window
+		window = SDL_CreateWindow( "Piano Tiles", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN );
+
+		if( window == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+			//Create renderer for window
+			renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED);
+			if( renderer == NULL)
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( renderer, 0x23, 0x98, 0xAB, 0xBC);
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+    return success;
+
+}
+
+bool Display::loadMedia(){
+    //Loading success flag
+	bool success = true;
+
+	//Nothing to load
+	return success;
+}
+
+void Display::close(){
+    //Destroy window
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	window = NULL;
+	renderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
 }
--- display.h
+++ display.h
@@ -1,12 +1,35 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include<SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include <cmath>
+
 
 class Display{
 
 private:
-    int a=10, b=20, c=30;
+
+    //The window we'll be rendering to
+    SDL_Window* window = NULL;
 
 public:
-    void show_test();
 
+    const int screenWidth=640;
+    const int screenHeight=480;
+
+    //The window renderer
+    SDL_Renderer* renderer = NULL;
+
+    //Starts up SDL and creates window
+    bool init();
+
+    //Loads media
+    bool loadMedia();
+
+    //Frees media and shuts down SDL
+    void close();
+
+    //Loads individual image as texture
+    SDL_Texture* loadTexture(std::string path);
 };
-
